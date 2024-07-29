6a4d5a4af956bf5a7568019005292d0043ec4b0f
jdapayne <jdapayne@gmail.com>
2024-07-29 12:10:36 +0100

    Use google web font

--- main.ts
+++ main.ts
@@ -2,12 +2,12 @@ import ModRing from './ModRing.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   // Get the input and canvas elements by their ids
-  const expressionInput = document.getElementById('expression') as HTMLInputElement|null;
-  const nInput = document.getElementById('n') as HTMLInputElement|null;
-  const singlePath = document.getElementById('path') as HTMLInputElement|null;
+  const expressionInput = document.getElementById('expression') as HTMLInputElement | null;
+  const nInput = document.getElementById('n') as HTMLInputElement | null;
+  const singlePath = document.getElementById('path') as HTMLInputElement | null;
   const canvas = document.getElementById('canvas') as HTMLCanvasElement;
   const modRing = new ModRing();
-  
+
   function getInfoAndDraw() {
     const expression = expressionInput?.value ?? 'n';
     const n = parseInt(nInput?.value ?? '10');
@@ -19,7 +19,7 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   document.getElementById('form')?.addEventListener('change', getInfoAndDraw);
-  
+
   getInfoAndDraw();
 });
 
